255ca267c8a75d8937343c077f98aa887a5ce4da
yaroshenkopavel <78513566+yaroshenkopavel@users.noreply.github.com>
2026-09-03 01:28:47 +0300

    Offline semantic provider: fix Android unit test annotation

--- android-offline-semantic-provider/src/test/kotlin/pro/liliya/android/semanticprovider/SemanticFlatIndexContractTest.kt
+++ android-offline-semantic-provider/src/test/kotlin/pro/liliya/android/semanticprovider/SemanticFlatIndexContractTest.kt
@@ -1,6 +1,6 @@
 package pro.liliya.android.semanticprovider
 
-import kotlin.test.Test
+import org.junit.Test
 import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
