c8b744aed6cd580d3f1b4b371772e61736d15e36
yaroshenkopavel <78513566+yaroshenkopavel@users.noreply.github.com>
2026-09-03 19:20:22 +0300

    Centralize semantic test artifact identity

---
+++ android-offline-semantic-provider/src/test/kotlin/pro/liliya/android/semanticprovider/TestSemanticModelArtifacts.kt
@@ -0,0 +1,31 @@
+package pro.liliya.android.semanticprovider
+
+import java.io.File
+
+internal object TestSemanticModelArtifacts {
+    fun validated(file: File): ValidatedSemanticModelArtifact = ValidatedSemanticModelArtifact(
+        file = file,
+        spec = SemanticModelArtifactSpec(
+            SemanticModelArtifactIdentity(
+                profileId = SemanticModelProfileV01.PROFILE_ID,
+                profileGeneration = SemanticModelProfileV01.PROFILE_GENERATION,
+                upstreamModelRepository = SemanticModelProfileV01.UPSTREAM_MODEL_REPOSITORY,
+                upstreamModelRevision = SemanticModelProfileV01.UPSTREAM_MODEL_REVISION,
+                conversionProvenance = SemanticConversionProvenance.Reproducible(
+                    artifactRepository = "test/reproducible-semantic-artifact",
+                    artifactRevision = "test-artifact-revision",
+                    conversionToolRevision = "test-conversion-tool-revision"
+                ),
+                ggufFileName = file.name,
+                expectedSizeBytes = 1L,
+                expectedSha256 = "0".repeat(64),
+                architecture = SemanticModelArchitecture.BERT,
+                embeddingDimension = SemanticModelProfileV01.EMBEDDING_DIMENSION,
+                poolingType = SemanticPoolingType.MEAN,
+                normalizationRule = SemanticNormalizationRule.L2,
+                tokenizerProfileId = SemanticModelProfileV01.TOKENIZER_PROFILE_ID,
+                llamaCppRevision = SemanticModelProfileV01.LLAMA_CPP_REVISION
+            )
+        )
+    )
+}
