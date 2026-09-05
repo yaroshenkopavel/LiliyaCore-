f29b8c5cab7dc31ddb8d70e5383a9c36637e48b1
yaroshenkopavel <78513566+yaroshenkopavel@users.noreply.github.com>
2026-09-05 18:06:05 +0300

    Create Android durable persistence module

---
+++ android-durable-persistence/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "pro.liliya.android.persistence"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 29
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":core"))
+
+    testImplementation(kotlin("test"))
+    testImplementation("junit:junit:4.13.2")
+
+    androidTestImplementation(kotlin("test"))
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test:runner:1.6.2")
+}
