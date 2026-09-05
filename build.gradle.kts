5e10a5ffc4f4ae00ea540845b7f864921d1d343c
yaroshenkopavel <78513566+yaroshenkopavel@users.noreply.github.com>
2026-09-06 02:19:13 +0300

    Add Android application plugin for Firebase test host

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
     kotlin("jvm") version "2.3.21" apply false
     id("com.android.library") version "9.3.0" apply false
+    id("com.android.application") version "9.3.0" apply false
 }
